95a37941f234f0832dde15cc2362cddbfefb3248
Wlad Kowalew <wladickowalew@gmail.com>
2020-04-05 14:38:10 +0300

    third task

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let vm = new Vue({
 	data:{
 		number:1,
 		thing: "",
-		list: ["сделать ДЗ", "Сидеть Дома", "Смотреть Netflix"]
+		list: ["сделать ДЗ", "Сидеть Дома", "Смотреть Netflix"],
+		numbers: ["1", "4"]
 	},
 	methods: {
 		double: function (event) {
@@ -11,13 +12,28 @@ let vm = new Vue({
 			this.number *= 2;
 		},
 		addItem: function (event){
-			if (event.which == 13) {
-				this.list.push(this.thing);
-				this.thing = "";
-			}
+			this.list.push(this.thing);
+			this.thing = "";
 		},
 		removeItem: function(index){
 			this.list.splice(index, 1);
 		}
+	},
+	computed:{
+		average: function(){
+			let sum = 0;
+			for (n of this.numbers) {
+				sum += Number(n);
+			}
+			return sum/this.numbers.length;
+		}
 	}
 })
+
+
+
+
+
+
+
+
