dd837546c361b203eda0c8c8b38135f0a1b16c4c
Wlad Kowalew <wladickowalew@gmail.com>
2020-04-05 14:16:20 +0300

    second task

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let vm = new Vue({
 	el: "#app",
 	data:{
-		number:1
+		number:1,
+		thing: "",
+		list: ["сделать ДЗ", "Сидеть Дома", "Смотреть Netflix"]
 	},
 	methods: {
 		double: function (event) {
 			console.log(event);
 			this.number *= 2;
+		},
+		addItem: function (event){
+			if (event.which == 13) {
+				this.list.push(this.thing);
+				this.thing = "";
+			}
+		},
+		removeItem: function(index){
+			this.list.splice(index, 1);
 		}
 	}
 })
